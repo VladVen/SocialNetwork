95e51351d9bf2732ee850b022fc086477e24051d
VladVen <venediktov.vlad@gmail.com>
2022-04-26 18:18:17 +0300

    24 lesson

--- src/components/dialogues/dialogues.jsx
+++ src/components/dialogues/dialogues.jsx
@@ -19,21 +19,38 @@ const Message = (props) => {
     )
 }
 
+
+
+
 const Dialogues = () => {
+
+    let DialoguesData = [
+        {id: 1, name: 'Jenya'},
+        {id: 2, name: 'Juliya'},
+        {id: 3, name: 'Vitalik'},
+        {id: 4, name: 'Egor'}
+    ]
+
+    let MessagesData = [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'How are you ?'},
+        {id: 3, message: 'Slava Ukraine!!!'},
+        {id: 4, message: 'Fuck off'}
+    ]
     return (
         <div className={style.dialogues}>
             <div className={style.dialoguesNames}>
-                <DialogueMembers name='Jenya' id='1'/>
-                <DialogueMembers name='Juliya' id='2'/>
-                <DialogueMembers name='Vitalik' id='3'/>
-                <DialogueMembers name='Egor' id='4'/>
+                <DialogueMembers name={DialoguesData[0].name} id={DialoguesData[0].id}/>
+                <DialogueMembers name={DialoguesData[1].name} id={DialoguesData[1].id}/>
+                <DialogueMembers name={DialoguesData[2].name} id={DialoguesData[2].id}/>
+                <DialogueMembers name={DialoguesData[3].name} id={DialoguesData[3].id}/>
 
             </div>
             <div className={style.messages}>
-               <Message message='Hi'/>
-               <Message message='How are you ?'/>
-               <Message message='Slava Ukraine'/>
-               <Message message='Fuck off'/>
+                <Message message={MessagesData[0].message} id={MessagesData[0].id}/>
+                <Message message={MessagesData[1].message} id={MessagesData[1].id}/>
+                <Message message={MessagesData[2].message} id={MessagesData[2].id}/>
+                <Message message={MessagesData[3].message} id={MessagesData[3].id}/>
             </div>
         </div>
     )
--- src/components/inform/MyPosts/myPosts.jsx
+++ src/components/inform/MyPosts/myPosts.jsx
@@ -3,6 +3,14 @@ import Post from './Post/post1'
 
 
 const myPosts = () => {
+
+    const PostData = [
+        {id:1, message: 'Hello world!!!',likes:85,dislikes:1 },
+        {id:2, message: 'Slava Ukraine',likes:105,dislikes:15 },
+        {id:3, message: 'Crimea is ours',likes:100,dislikes:5 },
+        {id:4, message: 'Русские пидоры',likes:456,dislikes:165 }
+    ]
+
    return ( 
      <div>
        <div>myPost</div>
@@ -10,8 +18,10 @@ const myPosts = () => {
              <textarea></textarea>
              <button>New Post</button>
            </div>
-         <Post message="Hello world!!!" likes="15" dislikes="1"/>
-         <Post message="I'm gay" likes="85" dislikes="5"/>
+         <Post id={PostData[0].id} message={PostData[0].message} likes={PostData[0].likes} dislikes={PostData[0].dislikes} />
+         <Post id={PostData[1].id} message={PostData[1].message} likes={PostData[1].likes} dislikes={PostData[1].dislikes} />
+         <Post id={PostData[2].id} message={PostData[2].message} likes={PostData[2].likes} dislikes={PostData[2].dislikes} />
+         <Post id={PostData[3].id} message={PostData[3].message} likes={PostData[3].likes} dislikes={PostData[3].dislikes} />
     </div>
    )
 }
