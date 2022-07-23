032ee5b9fdedc659765643bb93da5dd84dc033d6
vladVen <venediktov.vlad@gmail.com>
2022-07-23 13:53:39 +0300

    94 lesson

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import {Route, Routes} from "react-router-dom";
 import News from "./components/News/news";
 import Music from "./components/Music/music";
 import Settings from "./components/Settings/settings";
-import DialoguesContainer from "./components/dialogues/dialoguesContainer";
 import UsersContainer from "./components/Users/userContainer";
-import ProfileContainer from "./components/profile/profileContainer";
 import TopicContainer from "./components/topic/topicContainer";
 import LoginForm from "./components/Login/loginForm";
 import {connect} from "react-redux";
@@ -16,38 +14,45 @@ import withRouter from "./common/HOC/withRouter";
 import {runInitialize} from "./redux/reducers/appReducer";
 import Preloader from "./common/Preloader";
 
+const DialoguesContainer = React.lazy(() => import('./components/dialogues/dialoguesContainer'));
+const ProfileContainer = React.lazy(() => import('./components/profile/profileContainer'));
+
 
 class App extends React.Component {
     componentDidMount() {
         this.props.runInitialize()
     }
+
     render() {
         if (!this.props.initialized) {
-            return <div className='preloader'><Preloader /></div>
+            return <div className='preloader'><Preloader/></div>
         }
         return (
             <div className='app-wrapper'>
                 <TopicContainer/>
                 <SideBar/>
                 <div>
-                    <Routes>
-                        <Route path='/profile/:userId' element={<ProfileContainer/>}
-                        />
-                        <Route path='/profile/' element={<ProfileContainer/>}
-                        />
-                        <Route path='/dialogues/*' element={<DialoguesContainer/>}
-                        />
-                        <Route path='/news' element={<News/>}/>
-                        <Route path='/music' element={<Music/>}/>
-                        <Route path='/settings' element={<Settings/>}/>
-                        <Route path='/users/*' element={<UsersContainer/>}/>
-                        <Route path='/login' element={<LoginForm/>}/>
-                    </Routes>
+                    <React.Suspense fallback={<Preloader/>}>
+                        <Routes>
+                            <Route path='/profile/:userId' element={<ProfileContainer/>}
+                            />
+                            <Route path='/profile/' element={<ProfileContainer/>}
+                            />
+                            <Route path='/dialogues/*' element={<DialoguesContainer/>}
+                            />
+                            <Route path='/news' element={<News/>}/>
+                            <Route path='/music' element={<Music/>}/>
+                            <Route path='/settings' element={<Settings/>}/>
+                            <Route path='/users/*' element={<UsersContainer/>}/>
+                            <Route path='/login' element={<LoginForm/>}/>
+                        </Routes>
+                    </React.Suspense>
                 </div>
             </div>
         )
     }
 }
+
 const mapStateToProps = (state) => ({
     initialized: state.app.initialized
 })
