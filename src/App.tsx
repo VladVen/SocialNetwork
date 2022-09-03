5f67f82af90ddc2bf87630e7fcad136a66f3db86
VladVen <venediktov.vlad@gmail.com>
2022-09-03 10:51:30 +0300

    Ant Design integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,87 +1,147 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
-import {Navigate, Route, Routes} from "react-router-dom";
-import {connect} from "react-redux";
-import {compose} from "redux";
+import {Navigate, Route, Routes, useNavigate} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import {AnyAction} from "redux";
+import 'antd/dist/antd.css';
 
-import withRouter from "./common/HOC/withRouter";
 import {AppStateType} from "./redux/reduxStore";
 
 import {runInitialize} from "./redux/reducers/appReducer";
-import SideBar from './components/sideBar/sideBar';
 import News from "./components/News/news";
 import Music from "./components/Music/music";
 import Settings from "./components/Settings/settings";
 import LoginForm from "./components/Login/loginForm";
 import Preloader from "./common/Preloader";
 import ProfileContainer from "./components/profile/profileContainer";
+import {
+    FileOutlined,
+    HomeOutlined,
+    MessageOutlined,
+    SettingOutlined,
+    UploadOutlined,
+    UserAddOutlined
+} from '@ant-design/icons';
+import {Layout, Menu} from 'antd';
 import Topic from "./components/topic/topic";
-
+import {Footer} from "antd/es/layout/layout";
 
 const DialoguesContainer = React.lazy(() => import('./components/dialogues/dialoguesContainer'));
 const UsersPage = React.lazy(() => import('./components/Users/userContainer'));
 
+const {Header, Sider, Content} = Layout;
 
-type mapState = {initialized: boolean}
-type mapDispatch = {runInitialize: () => void}
 
-type Props = mapDispatch & mapState
+type Props = {}
 
-class App extends React.Component<Props> {
-    catchAllUnhandledErrors = (promiseRejected: PromiseRejectionEvent) => {
+const App: React.FC<Props> = () => {
+    const [collapsed, setCollapsed] = useState(false);
+    const initialized = useSelector((state: AppStateType) => state.app.initialized)
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const catchAllUnhandledErrors = (promiseRejected: PromiseRejectionEvent) => {
         alert('Turn on vpn')
     }
-    componentDidMount() {
-        this.props.runInitialize()
-        window.addEventListener('unhandledrejection', this.catchAllUnhandledErrors)
-    }
-    componentWillUnmount() {
-        window.removeEventListener('unhandledrejection', this.catchAllUnhandledErrors)
+    useEffect(() => {
+        dispatch(runInitialize() as unknown as AnyAction)
+        window.addEventListener('unhandledrejection', catchAllUnhandledErrors)
+
+        return window.removeEventListener('unhandledrejection', catchAllUnhandledErrors)
+    }, [])
+
+    if (!initialized) {
+        return <div className='preloader'><Preloader/></div>
     }
 
-    render() {
-        if (!this.props.initialized) {
-            return <div className='preloader'><Preloader/></div>
-        }
-        return (
-            <div className='app-wrapper'>
-                <Topic/>
-                <SideBar/>
-                <div>
-                    <React.Suspense fallback={<Preloader/>}>
-                        <Routes>
-
-                            <Route path='/profile/:userId' element={<ProfileContainer/>}
-                            />
-                            <Route path='/profile/' element={<ProfileContainer/>}
-                            />
-                            <Route path='/dialogues/*' element={<DialoguesContainer/>}
-                            />
-                            <Route path='/news' element={<News/>}/>
-                            <Route path='/music' element={<Music/>}/>
-                            <Route path='/settings' element={<Settings/>}/>
-                            <Route path='/users/*' element={<UsersPage/>}/>
-                            <Route path='/login' element={<LoginForm/>}/>
-                            <Route path='*' element={<div><b>404 not Found</b></div>}/>
-                            <Route path='/' element={<Navigate to="/profile" />}
-                            />
-                        </Routes>
-                    </React.Suspense>
+    return (
+        <Layout >
+            <Sider trigger={null} collapsible collapsed={collapsed}>
+                <div className="logo">
+                    <img className="img" src='https://v.od.ua/uploads/92/logo.png'/>
                 </div>
-            </div>
-        )
-    }
+                <Menu
+                    theme="dark"
+                    mode="inline"
+                    defaultSelectedKeys={['1']}
+                    items={[
+                        {
+                            key: '1',
+                            icon: <HomeOutlined />,
+                            label: 'My Profile',
+                            onClick: () =>{navigate('/profile')}
+                        },
+                        {
+                            key: '2',
+                            icon: <MessageOutlined />,
+                            label: 'Dialogs',
+                            onClick: () =>{navigate('/dialogues')}
+                        },
+                        {
+                            key: '3',
+                            icon: <FileOutlined />,
+                            label: 'News',
+                            onClick: () =>{navigate('/news')}
+                        },
+                        {
+                            key: '4',
+                            icon: <UploadOutlined/>,
+                            label: 'Music',
+                            onClick: () =>{navigate('/music')}
+                        },
+                        {
+                            key: '5',
+                            icon: <UserAddOutlined />,
+                            label: 'Users',
+                            onClick: () =>{navigate('/users')}
+                        },
+                        {
+                            key: '6',
+                            icon: <SettingOutlined />,
+                            label: 'Settings',
+                            onClick: () =>{navigate('/settings')}
+                        },
+                    ]}
+                />
+            </Sider>
+            <Layout className="site-layout">
+              <Topic collapsed={collapsed} setCollapsed={setCollapsed} />
+                <Content
+                    className="site-layout-background"
+                    style={{
+                        padding: 24,
+                        minHeight: 280,
+                    }}
+                >
+                        <React.Suspense fallback={<Preloader/>}>
+                            <Routes>
+
+                                <Route path='/profile/:userId' element={<ProfileContainer/>}
+                                />
+                                <Route path='/profile/' element={<ProfileContainer/>}
+                                />
+                                <Route path='/dialogues/*' element={<DialoguesContainer/>}
+                                />
+                                <Route path='/news' element={<News/>}/>
+                                <Route path='/music' element={<Music/>}/>
+                                <Route path='/settings' element={<Settings/>}/>
+                                <Route path='/users/*' element={<UsersPage/>}/>
+                                <Route path='/login' element={<LoginForm/>}/>
+                                <Route path='*' element={<div><b>404 not Found</b></div>}/>
+                                <Route path='/' element={<Navigate to="/profile" />}
+                                />
+                            </Routes>
+                        </React.Suspense>
+                </Content>
+                <Footer style={{textAlign: "center"}}>Developed by <a href={'https://github.com/VladVen'} target={'_blank'}>VladVen</a></Footer>
+            </Layout>
+        </Layout>
+
+
+    )
+
 }
 
-const mapStateToProps = (state: AppStateType) => ({
-    initialized: state.app.initialized
-})
 
-const mapDispatchToProps = {
-    runInitialize,
-}
+export default App
 
-export default compose<React.ComponentType>(
-    withRouter,
-    connect(mapStateToProps, mapDispatchToProps))
-(App)
--- src/components/topic/topic.tsx
+++ src/components/topic/topic.tsx
@@ -1,15 +1,23 @@
-import style from './topic.module.css'
 import {NavLink} from "react-router-dom";
 import React from "react";
 import {useDispatch, useSelector} from "react-redux";
-import {getIsAuth, getLogin} from "../../redux/selectors/authSelector";
+import {getIsAuth} from "../../redux/selectors/authSelector";
 import {logOutTC} from "../../redux/reducers/authReducer";
 import {AnyAction} from "redux";
+import {MenuFoldOutlined, MenuUnfoldOutlined} from "@ant-design/icons";
+import {Avatar, Button} from "antd";
+import {Header} from "antd/es/layout/layout";
+import {AppStateType} from "../../redux/reduxStore";
+import 'antd/dist/antd.css';
 
 
+type Props = {
+    collapsed: boolean
+    setCollapsed: (collapsed: boolean) => void
+}
 
-const Topic: React.FC = () => {
-    const login = useSelector(getLogin)
+const Topic: React.FC<Props> = ({collapsed, setCollapsed}) => {
+    const photo = useSelector((state: AppStateType) => state.profilePage.profileData?.photos.small)
     const isAuth = useSelector(getIsAuth)
     const dispatch = useDispatch()
 
@@ -18,18 +26,24 @@ const Topic: React.FC = () => {
     }
 
     return (
-        <header className={style.topic}>
-       <img className={style.logo} src='https://v.od.ua/uploads/92/logo.png'/>
-            <div className={style.login}>
-            { isAuth
-                ? <div>
-                    {login}
-                    <button onClick={logOut}>Log out</button>
-            </div>
-                : <NavLink to={'/login'}>Login</NavLink>
+
+        <Header className="site-layout-background" style={{paddingTop: 0, display: "flex", flex: 1, alignItems: "center"}}>
+            {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+                className: 'trigger',
+                style:{fontSize: '20px'},
+                onClick: () => setCollapsed(!collapsed),
+            })}
+            {
+                isAuth
+                    ? <span style={{marginLeft: "auto", alignItems: "center"}}>
+                        <Avatar src={photo} />
+                    <Button onClick={logOut} style={{marginLeft: '20px'}}>Log Out</Button>
+                    </span>
+                    : <NavLink to={'/login'} style={{marginLeft: "auto", alignItems: "center"}}><Button>Login</Button></NavLink>
             }
-            </div>
-        </header>
+
+        </Header>
+
     )
 }
 export default Topic
